cdc05c38643209fc51ce789a2ba02dada17a8999
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-07-12 15:54:10 +0900

    [UPDATE] Header - Button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import styled from "styled-components";
 import Header from "./components/Header/Header";
 
+const GlobalStyleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 function App() {
-  return <Header />;
+  return (
+    <GlobalStyleBox>
+      <Header />
+    </GlobalStyleBox>
+  );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,8 @@ const HeaderBlock = styled.div`
   width: 100%;
   height: 40px;
   border-bottom: 2px solid #bcbcbc;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Logo = styled.img`
@@ -12,10 +14,26 @@ const Logo = styled.img`
   height: 40px;
 `;
 
+const ButtonBox = styled.div`
+  width: 150px;
+  height: 40px;
+  display: flex;
+`;
+
+const LogRegButton = styled.button`
+  width: 100px;
+  height: 30px;
+  margin-right: 5px;
+`;
+
 const Header = () => {
   return (
     <HeaderBlock>
       <Logo src={logo} />
+      <ButtonBox>
+        <LogRegButton>로그인</LogRegButton>
+        <LogRegButton>회원가입</LogRegButton>
+      </ButtonBox>
     </HeaderBlock>
   );
 };
