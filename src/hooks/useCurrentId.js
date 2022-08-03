b6cbf449094d3253959be5e6722517ed4bbe1b91
시윤 <>
2022-08-03 17:40:57 +0900

    [FIX] merge 버그 수정

--- src/hooks/useCurrentId.js
+++ src/hooks/useCurrentId.js
@@ -1,8 +1,8 @@
 import useFetch from '../hooks/useFetch';
 
 function useCurrentId(url) {
-  const fetch = useFetch(url);
-  const posts = [...fetch]
+  const fetchs = useFetch(url);
+  const posts = [...fetchs]
   const idList = posts.map((post) => post.id)
   const id = Math.max(...idList);
   return id;
